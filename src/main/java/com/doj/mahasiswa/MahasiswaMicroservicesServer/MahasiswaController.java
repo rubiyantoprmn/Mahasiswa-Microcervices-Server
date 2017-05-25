a3cf24c0a5922a4049bd2c401cf83d24d745269a
triayudap <triayuda.p@gmail.com>
2017-05-25 14:45:54 +0700

    Update Mahasiswa Controller

--- src/main/java/com/doj/mahasiswa/MahasiswaMicroservicesServer/MahasiswaController.java
+++ src/main/java/com/doj/mahasiswa/MahasiswaMicroservicesServer/MahasiswaController.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,6 +14,7 @@ import com.doj.mahasiswa.MahasiswaMicroservicesServer.MahasiswaRepository;
 import com.doj.mahasiswa.MahasiswaMicroservicesServer.Mahasiswa;
 
 @RestController
+@RequestMapping("/mahasiswa")
 public class MahasiswaController {
 	
 	protected Logger logger = Logger.getLogger(MahasiswaController.class.getName());
@@ -20,21 +22,21 @@ public class MahasiswaController {
 	@Autowired
 	MahasiswaRepository repository;
 	
-	@RequestMapping("/mahasiswa")
-	public Mahasiswa[] All()
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Mahasiswa> findAll()
 	{
-		logger.info("mahasiswa-microservice all() invoked");
+		//logger.info("mahasiswa-microservice all() invoked");
 		List<Mahasiswa> mahasiswa = MahasiswaRepository.getAllMahasiswa();
-		logger.info("mahasiswa-microservice all() found: " + mahasiswa.size());
-		return mahasiswa.toArray(new Mahasiswa[mahasiswa.size()]);
+		//logger.info("mahasiswa-microservice all() found: " + mahasiswa.size());
+		return mahasiswa;
 	}
 	
-	@RequestMapping("/mahasiswa/{id}")
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Mahasiswa ById(@PathVariable("nim_mahasiswa") String id)
 	{
-		logger.info("mahasiswa-microservice byId() invoked: " + id);
+		//logger.info("mahasiswa-microservice byId() invoked: " + id);
 		Mahasiswa mahasiswa = MahasiswaRepository.getMahasiswa(id);
-		logger.info("mahasiswa-microservice byId() found: " + mahasiswa);
+		//logger.info("mahasiswa-microservice byId() found: " + mahasiswa);
 		return mahasiswa;
 	}
 }
